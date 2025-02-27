ce21ef771655013facf09da896f114a7586c2b7b
Kevin-Pember <Kevinkzp@gmail.com>
2025-02-27 12:11:35 -0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	
+</script>
+
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<div id="mainBody" class=" w-full md:w-[60%]">
+		<math-field class=" bg-[var(--secondary)] w-full " data-gramm='false'></math-field>
+	</div>
+</section>
+
+<style>
+	
+</style>
